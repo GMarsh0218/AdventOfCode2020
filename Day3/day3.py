5243f9985f71392a808e92866319765597dbbd5d
Gort <gmarsh0218@gmail.com>
2020-12-04 03:13:21 -0500

    Day 3 solution in python

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,11 +1,28 @@
-
-#  # == tree
-# . == open square
-
 with open("input.txt") as fo:
-    lines = [line.splitlines() for line in fo]
+    treeMap = [[c for c in line.rstrip()] for line in fo]
 
-print(lines)
-treeMap = [[1 if c == '#' else 0 for c in line] for line in lines]
 
-print(treeMap)
+def traverseMap(slope):
+    pos = slope[0]
+    treesHit = 0
+
+    for i in range(slope[1], len(treeMap), slope[1]):
+        row = treeMap[i]
+        if row[pos] == "#":
+            treesHit += 1
+        pos = (pos + slope[0]) % len(row)  # adds right val to x and makes sure it doesnt go out of bounds
+
+    return treesHit
+
+
+def part2():
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]  # (right, down)
+    x = traverseMap(slopes[0])
+    for i in range(1, len(slopes)):
+        x = x * traverseMap(slopes[i])
+    return x
+
+
+if __name__ == "__main__":
+    print("Part 1: ", traverseMap((3, 1)))
+    print("Part 2: ", part2())
